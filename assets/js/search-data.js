c744e3f8b813b2e2b4c5224c08a81046662a0c5b
yiding-ma <62972379+yiding-ma@users.noreply.github.com>
2026-06-29 15:35:37 +0000

    Deploying to gh-pages from @ yiding-ma/yiding-ma.github.io@10b228d35ce2e323dfb4bcf5ebc5249d6c5495a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "[**Curriculum Vitae**](/assets/pdf/YidingMa_CV.pdf)",
+          description: "[**Curriculum Vitae**](/assets/pdf/YidingMa_CV_06292026.pdf)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
